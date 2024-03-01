7f556c4f7ddab6796b939e8ee4344bbcf7ae9771
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-03-01 15:01:21 +0000

    Update heroku url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://ci-walkthrough-rest-0d3c72236631.herokuapp.com/";
+axios.defaults.baseURL = "https://bakers-drfapi-f4c1c693be28.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
